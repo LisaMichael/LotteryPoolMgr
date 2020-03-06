9e2a7fe6d7e7a43b1cb8081a4dcb6dd84932f7c6
Lisa <lisa.michael@hotmail.com>
2020-03-06 08:14:25 -0500

    added header component

---
+++ client/src/components/Header/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "../Header/style.css";
+
+function Header() {
+    return (
+        <div>
+            <header className="header">
+                <h1>Lottery Pool</h1>
+                <h2>Can't Win if you don't Play !</h2>
+            </header>
+        </div>
+    );
+}
+
+export default Header;
