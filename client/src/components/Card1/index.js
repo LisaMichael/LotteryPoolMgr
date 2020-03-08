c09e2662ead36097846c74e90bbfe5f7272c876b
Lisa <lisa.michael@hotmail.com>
2020-03-08 18:25:40 -0400

    changed the size of image for login

--- client/src/components/Card1/index.js
+++ client/src/components/Card1/index.js
@@ -1,22 +1,30 @@
 
 import React from 'react';
+import "../Card1/style.css";
+
+// import the image to be used by the card
 import imglogin from "../../assets/images/pbLogin.jpg";
+
+
+// function created to define the card 
+// instructions are provided in this youtube video: 
+// https://www.youtube.com/watch?v=rH9jM-8hAD8
 
 const Card = props => {
 
     return (
         <div className="card text-center">
             <div className="overflow">
-                <img src={imglogin} alter="imagelogin" />
+                <img src={imglogin} alter="imagelogin" style={{height: '100px'}} />
             </div>
 
             <div className="card-body text-dark">
                 <h4 className="card-title">YOU MUST BE A MEMBER TO ACCESS</h4>
                 <p className="card-text text-secondary"></p>
-                <a href="#" className ="btn btn-outline-success">CLICK TO LOGIN</a>
+                <a href="#" className="btn btn-outline-success">CLICK TO LOGIN</a>
             </div>
-            </div>
-            );
-          }
-          
+        </div>
+    );
+}
+
 export default Card; 
